2ee7b66d9178baae529e08bc25f8ba6bb50bb8c2
mbcgo12 <76557695+mbcgo12@users.noreply.github.com>
2021-07-15 17:17:46 +0900

    particle

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
+// import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 
 import './App.css';
 import 'tachyons';
 
+
+// const ParticlesOptions = {
+//     particles: {
+//       line_linked: {
+//         shadow: {
+//           enable: true,
+//           color: "#3CA9D1",
+//           blur: 5
+//         }
+//       }
+//     }
+// }
+
+
 function App() {
   return (
     <div className="App">
+     {/* <Particles params={ParticlesOptions}/>  */}
      <Navigation/>
      <Logo />
-     {/* <ImageLinkForm />
-     <FaceRecognition /> */}
+     <Rank />
+     <ImageLinkForm />
+     {/* <FaceRecognition /> */}
     </div>
   );
 }
