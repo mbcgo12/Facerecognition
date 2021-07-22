ebd1ecbbf601cf9939acaa9521f72644083c1b5a
mbcgo12 <76557695+mbcgo12@users.noreply.github.com>
2021-07-22 23:02:10 +0900

    sign in

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Signin from './components/Signin/Signin';
 
 import './App.css';
 import 'tachyons';
@@ -35,6 +36,7 @@ class App extends Component {
       input:'',
       imageUrl:'',
       box:{},
+      route:'signin',
     }
   }
 
@@ -70,18 +72,27 @@ class App extends Component {
       .catch(err => console.log(err));
   }
 
+  onRouteChange = (route) =>{
+    this.setState({route: route});
+
+  }
+
   render(){
   return (
     <div className="App">
      <Particles className='particles' params={ParticlesOptions}/> 
-     <Navigation/>
-     <Logo />
-     <Rank />
-     <ImageLinkForm 
-      onInputChange={this.onInputChange} 
-      onButtonSubmit={this.onButtonSubmit}
-     />
-     <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+     <Navigation onRouteChange={this.onRouteChange}/>
+     { this.state.route === 'signin'
+      ?<Signin onRouteChange={this.onRouteChange}/>
+      :<div><Logo />     
+        <Rank />
+        <ImageLinkForm 
+        onInputChange={this.onInputChange} 
+        onButtonSubmit={this.onButtonSubmit}
+        />
+       <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+      </div>
+    }
     </div>
   );
   }
